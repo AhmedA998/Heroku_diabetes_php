f1d4fa0f02c9f2da6979b2a4b1849a0a9a6a7171
AhmedA998 <62741306+AhmedA998@users.noreply.github.com>
2020-07-19 15:50:13 +0200

    Update app.js

--- templates/app.js
+++ templates/app.js
@@ -1,41 +1,44 @@
-
 var db = firebase.firestore();
-function send(){
-const gender = document.querySelector("#select1").value;
-const agetxt = document.querySelector("#age").value;
-const select2txt = document.querySelector("#select2").value;
-const cigtxt = document.querySelector("#cig").value;
-const address2txt = document.querySelector("#address2").value;
-const select3txt = document.querySelector("#select3").value;
-const select4txt = document.querySelector("#select4").value;
-const select5txt = document.querySelector("#select5").value;
-const choltxt = document.querySelector("#chol").value;
-const pretxt = document.querySelector("#pre").value;
-const dbltxt = document.querySelector("#dbl").value;
-const bmitxt = document.querySelector("#bmi").value;
-const hrttxt = document.querySelector("#hrt").value;
-const glutxt = document.querySelector("#glu").value;
+
+function send() {
+    const gender = document.querySelector("#select1").value;
+    const agetxt = document.querySelector("#age").value;
+    const select2txt = document.querySelector("#select2").value;
+    const cigtxt = document.querySelector("#cig").value;
+    const address2txt = document.querySelector("#address2").value;
+    const select3txt = document.querySelector("#select3").value;
+    const select4txt = document.querySelector("#select4").value;
+    const select5txt = document.querySelector("#select5").value;
+    const choltxt = document.querySelector("#chol").value;
+    const pretxt = document.querySelector("#pre").value;
+    const dbltxt = document.querySelector("#dbl").value;
+    const bmitxt = document.querySelector("#bmi").value;
+    const hrttxt = document.querySelector("#hrt").value;
+    const glutxt = document.querySelector("#glu").value;
+
+    db.collection("features").doc().set({
+            Gender: gender,
+            Age: agetxt,
+            currentsmoker: select2txt,
+            CigarettesPerDay: cigtxt,
+            BloodPressure: address2txt,
+            prevalentStroke: select3txt,
+            prevalentHyp: select4txt,
+            diabetes: select5txt,
+            totChol: choltxt,
+            sysBP: pretxt,
+            DiastolicBlood: dbltxt,
+            BodyMassIndex: bmitxt,
+            heartRate: hrttxt,
+            glucose: glutxt
+
+        })
+        .then(function() {
+            alert("Data added");
+        })
+        .catch(function(error) {
+            alert("Error written document", error);
+
+        });
+
 }
- db.collection("features").doc().set({
-                Gender: select1,
-                Age: age,
-                currentsmoker: select2,
-                CigarettesPerDay: cig,
-                BloodPressure: address2,
-                prevalentStroke: select3,
-                prevalentHyp: select4,
-                diabetes: select5,
-                totChol: chol,
-                sysBP: pre,
-                BodyMassIndex: dbl,
-                heartRate: hrt,
-                glucose: glu,
-
-            })
-            .then(function() {
-                alert("Data added");
-            })
-            .catch(function(error) {
-                alert("Error written document", error);
-
-            });
